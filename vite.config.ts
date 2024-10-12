e1c2d84c984b9c29f4bdd6e4b37f64a89a156fe7
ch1nru5t <chinrust@gmail.com>
2024-10-12 20:31:14 +0530

    chore: update iterate vite plugin URL

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,16 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
-import { iterateVitePLugin } from '@iterate-ai/viteplugin';
-import { defineConfig } from "vite"
+import { iterateVitePLugin } from "@iterate-ai/viteplugin";
+import react from "@vitejs/plugin-react";
+import path from "path";
+import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [react(), iterateVitePLugin('https://s1lgzjst-8000.inc1.devtunnels.ms')],
+  plugins: [react(), iterateVitePLugin("https://dev-api.iterate-ai.com")],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
   build: {
-    sourcemap: true
-  }
-})
+    sourcemap: true,
+  },
+});
