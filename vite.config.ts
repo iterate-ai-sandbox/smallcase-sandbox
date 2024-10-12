684ecbb607a3f4916ef8e2df08c1437d5496d323
ch1nru5t <chinrust@gmail.com>
2024-10-12 20:41:21 +0530

    chore: update iterate vite plugin URL

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,10 @@ import path from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [react(), iterateVitePLugin("https://dev-api.iterate-ai.com")],
+  plugins: [
+    react(),
+    iterateVitePLugin("https://dev-api.iterate-ai.com/api/v0/source-maps"),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
