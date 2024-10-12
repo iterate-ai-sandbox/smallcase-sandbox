d2e2dcb2aa4e3a77125b3008c8c7bf43ece71977
phukon <hello@rikiphukon.com>
2024-10-12 19:14:43 +0530

    feat: add iterate vite plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,16 @@
 import path from "path"
 import react from "@vitejs/plugin-react"
+import { iterateVitePLugin } from '@iterate-ai/viteplugin';
 import { defineConfig } from "vite"
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), iterateVitePLugin('https://s1lgzjst-8000.inc1.devtunnels.ms')],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  build: {
+    sourcemap: true
+  }
 })
